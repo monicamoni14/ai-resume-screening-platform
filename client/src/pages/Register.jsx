265d2dd0672c10a733b2abfa9ee2e052cc37900a
monicamoni14 <monicadevi2314@gmail.com>
2026-06-14 20:53:14 +0530

    feat: add register form and validation

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleRegister = () => {
+  if (
+    name === "" ||
+    email === "" ||
+    password === "" ||
+    confirmPassword === ""
+  ) {
+    alert("Please fill all fields");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match");
+    return;
+  }
+
+  alert("Registration Successful");
+};
+
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>Register</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(event) => setConfirmPassword(event.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleRegister}>Register</button>
     </div>
   );
 }
