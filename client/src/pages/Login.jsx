8ad7eb997bbf78fbc25a4b6e6b829e9761cd1a87
monicamoni14 <monicadevi2314@gmail.com>
2026-06-14 20:46:07 +0530

    feat: add login form and validation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+  if (email === "") {
+    alert("Please enter email");
+    return;
+  }
+
+  if (password === "") {
+    alert("Please enter password");
+    return;
+  }
+
+  alert("Login Successful");
+};
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>Login</h1>
+
+      <p>Welcome Back!</p>
+
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleLogin}>Login</button>
+
+      <hr />
+
+      <p>Email: {email}</p>
+      <p>Password: {password}</p>
     </div>
   );
 }
