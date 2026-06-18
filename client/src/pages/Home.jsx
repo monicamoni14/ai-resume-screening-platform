19e58cada7cf67fbbf82ad3bf99eec15bd9f5433
monicamoni14 <monicadevi2314@gmail.com>
2026-06-19 01:46:05 +0530

    feat: connect React frontend with Express backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 function Home() {
+  const [message, setMessage] = useState("");
+  const getMessage = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/message");
+
+    const data = await response.json();
+
+    alert(data.message);
+
+    setMessage(data.message);
+  } catch (error) {
+    alert(error.message);
+  }
+};
   return (
     <div>
       <h1>AI Resume Screening Platform</h1>
@@ -12,6 +28,12 @@ function Home() {
       <Link to="/register">
         <button>Register</button>
       </Link>
+       <br />
+       <br />
+       <button onClick={getMessage}>
+         Get Backend Message
+       </button>
+       <h2>{message}</h2>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello from Backend");
+app.use(cors());
+app.get("/message", (req, res) => {
+  console.log("Frontend requested /message");
+
+  res.json({
+    message: "Hello from Backend",
+  });
 });
 
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("MONICA SERVER IS RUNNING");
 });
+
+setInterval(() => {
+  console.log("Still Alive");
+}, 5000);
